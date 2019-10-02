f904b1aa12a81103a96d44fcb7f1aa0b0b000e16
Alexandr Lukovnikov <alexandr.lukovnikov@whenspeak.ru>
2019-10-02 14:21:25 +0300

    default test

---
+++ Tests/template-test-node-v8.c
@@ -0,0 +1,35 @@
+/*
+ * TrainFramework test
+ * {{ name }}
+ * {{ date }}
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "core-train.h"
+#include "core-routes.h"
+#include "core-utils.h"
+
+{%for module in modules.modules%}#include "{{module}}.h"
+{%endfor%}
+
+
+int main(void) {
+
+	initAdapter();
+
+	{% for module in modules.modules %}{% if modules[module].station.name %}
+	{{ modules[module].station.name }}Init();{% endif %}{% endfor %}
+
+	
+	for(;;) {
+		dbStation(NULL);
+		{% for module in modules.modules %}{% if modules[module].station.name %}
+		{{ modules[module].station.name }}(NULL);{% endif %}{% endfor %}
+	}
+
+	return EXIT_SUCCESS;
+}
+
